2d11323fa73c7adef9e81cc871724df3ff3b3cc7
Khan664 <108129645+Khan664@users.noreply.github.com>
2022-06-26 09:20:50 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['923187028664']
+global.premium = ['923187028664']
+global.ownernomer = '923187028664'
+global.ownername = devil king
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
